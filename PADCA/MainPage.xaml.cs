baecace743117f4ec1cf983041026bef2ca1c3fe
thorskii92 <nestorigna92@gmail.com>
2023-08-24 14:28:32 +0800

    Add project files.

---
+++ PADCA/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace PADCA
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NavToObservation(object sender, EventArgs e)
+        {
+            return;
+        }
+    }
+}
